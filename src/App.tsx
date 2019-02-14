96dd5ab6674cf1ff15ab3d8a59f72fa1a41ea494
stephanwallentin <stephan.wallentin@gmail.com>
2019-02-14 19:56:16 +0100

    boilerplate with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, HTMLProps } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+
+const Test: React.FunctionComponent<HTMLProps<HTMLDivElement>> = ({
+  className
+}) => <div className={className}>Testing</div>;
+
+const StyledComponent = styled(Test)`
+  font-size: 12em;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <StyledComponent />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
